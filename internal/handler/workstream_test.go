b183569aae05b5cec88d8cb6cad481df3a1530ea
raimajo <roboturnerdev@gmail.com>
2025-05-04 01:05:02 -0400

    test notes

--- internal/handler/workstream_test.go
+++ internal/handler/workstream_test.go
@@ -50,10 +50,12 @@ func (m *mockWorkstreamDB) DeleteWorkstream(id int) error {
 }
 
 func TestCreateNewPost_Success(t *testing.T) {
+
+	// Arrange
 	
+	// - make a dummy db
 	called := false
 	var savedWorkstream models.Workstream
-
 	mockDB := &mockWorkstreamDB{
 		CreateWorkstreamFunc: func(ws models.Workstream) error {
 			called = true
@@ -62,9 +64,13 @@ func TestCreateNewPost_Success(t *testing.T) {
 		},
 	}
 
+	// set up dependencies to implement a mock handler
 	logger := log.New(io.Discard, "", 0)
+
+	// mock the handler with dummy logger and db
 	h := handler.NewWorkstreamHandler(logger, mockDB)
 
+	// create a body to send to the mock post endpoint handler
 	form := url.Values{}
 	form.Add("name", "Test Workstream")
 	form.Add("code", "ABC123")
@@ -73,19 +79,28 @@ func TestCreateNewPost_Success(t *testing.T) {
 	form.Add("identity", "DevOps")
 	form.Add("quote", "Ship it!")
 
+	// Act
+	
+	// build the request
 	req := httptest.NewRequest(http.MethodPost, "/workstreams/new", strings.NewReader(form.Encode()))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	w := httptest.NewRecorder()
 
+	// send request and capture response
 	h.CreateNewPost(w, req)
-
 	res := w.Result()
+
+	// Assert
+
+	// case: did not get redirected
 	if res.StatusCode != http.StatusFound {
 		t.Errorf("expected redirect (302), got %d", res.StatusCode)
 	}
+	// case: CreateWorkstream did not get called
 	if !called {
 		t.Fatal("expected CreateWorkstream to be called")
 	}
+	// case: resulting new workstream name didnt match expected 
 	if savedWorkstream.Name != "Test Workstream" {
 		t.Errorf("expected name to be 'Test Workstream', got '%s'", savedWorkstream.Name)
 	}
